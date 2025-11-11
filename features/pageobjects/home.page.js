807652061c3f886da2aac215f08fbfdfe7f13c5c
Oksana <lug.oksana.l@gmail.com>
2025-11-12 01:26:13 +0200

    add "acceptCookiesIfPresent"

--- features/pageobjects/home.page.js
+++ features/pageobjects/home.page.js
@@ -15,9 +15,14 @@ class HomePage extends Page {
   }
 
   async clickCallYourAgent() {
-    await this.callYourAgentButton.scrollIntoView();
-    await this.callYourAgentButton.waitForClickable();
-    await this.callYourAgentButton.click();
+    await this.acceptCookiesIfPresent();
+    const button = await $('a[href="#interactive-tool-demo"]');
+    await button.scrollIntoView();
+    await button.waitForClickable({ timeout: 5000 });
+    await button.click();
+    // await this.callYourAgentButton.scrollIntoView();
+    // await this.callYourAgentButton.waitForClickable();
+    // await this.callYourAgentButton.click();
   }
 
   async isInteractiveToolVisible() {
@@ -29,6 +34,14 @@ class HomePage extends Page {
     const title = await browser.getTitle();
     return title.includes('Telnyx');
   }
+
+  async acceptCookiesIfPresent() {
+    const cookieAcceptButton = await $('#onetrust-accept-btn-handler');
+    if (await cookieAcceptButton.isExisting()) {
+        await cookieAcceptButton.click();
+        await browser.pause(500); // невелика пауза, щоб банер зник
+    }
+}
 
 }
 
